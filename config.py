282d60e05c8572bc137f6cb9ad64f03a330ea295
kristina378 <kristinadubiaha@gmail.com>
2025-10-28 21:40:23 +0100

    Add the first things to repository

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    WEATHER_API_KEY = "280479892b78ef9125fb7da96fc6b5bc"
+    CITY = "Lisbon"
+    EXCEL_FILEPATH = "pogoda.xlsx"
+
+testowa = 10
---
+++ main.py
@@ -0,0 +1,14 @@
+from config import Config,testowa
+
+print(Config.CITY)
+print(testowa)
+
+
+# -- GIT --
+#git init
+#git status
+#git add .
+#git commit -m "Wiadomosc"
+#git config --local/global user.name/email
+#git checkout  commits-id
+#git checkout -  -- powrot do "terazniejszosci"
